990d119f44cf64a124676d5c50f3824564cd1588
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-17 00:18:31 +0000

    updating

--- exercises/19-Looping-With-FizzBuzz/app.py
+++ exercises/19-Looping-With-FizzBuzz/app.py
@@ -1,5 +1,11 @@
 def fizz_buzz():
     # your code here
-
+    for i in range(1, 100):
+        if i % 3 == 0 and i % 5 == 0:
+            return "FizzBuzz"
+        elif i % 3 == 0:
+            return "Fizz"
+        elif i % 5 == 0:
+            return "Buzz"
 
 fizz_buzz()
