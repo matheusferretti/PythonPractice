457ee56a8b470821986e4428a7ce4c4309d596bb
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-17 00:38:09 +0000

    updating

--- exercises/21-The-Beatles/app.py
+++ exercises/21-The-Beatles/app.py
@@ -1,6 +1,13 @@
 # Your code here!!
+def sing():
+    for i in range(0, 12):
+        if i == 4:
+            print("whisper words of wisdom,")
+        elif i == 10:
+            print("there will be an answer,")
+        elif i == 11:
+            print("let it be")
+        else:
+            print("let it be,")
 
-
-
-
-
+sing()
