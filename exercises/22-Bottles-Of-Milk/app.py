ef3b34f3bb98ba97c91f15d564c0906101bba469
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-19 20:41:09 +0000

    updated

--- exercises/22-Bottles-Of-Milk/app.py
+++ exercises/22-Bottles-Of-Milk/app.py
@@ -1,10 +1,10 @@
 def number_of_bottles():
     for i in reversed(range(0, 100)):
         if i == 1:
-            print(str(i) + " bottle of milk on the wall, " + str(i) + " bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.")
+            print(str(i) + str(" bottle of milk on the wall 1 bottle of milk. Take one down and pass it around, no more bottles of milk on the wall."))
         elif i == 0:
             print("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.")
         else:
-            print(str(i) + " bottles of milk on the wall, " + str(i) + " bottles of milk. Take one down and pass it around, " + str(i - 1) + " bottles of milk on the wall.")
-
+            print(str(f"{i} bottles of milk on the wall, {i} bottles of milk. Take one down and pass it around, {i-1} bottles of milk on the wall."))
+            
 number_of_bottles()
